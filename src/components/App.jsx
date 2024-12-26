7d18d3648c35bc9fa5a0022c312b2befb129f52a
Jacob Silverman <57313642+jacobsilverman@users.noreply.github.com>
2024-12-25 22:27:30 -0800

    fixing refresh page problem and adding to the homepage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -47,7 +47,28 @@ function App() {
 }
 
 function Home() {
-  return <div>Welcome to the IdaDog!</div>;
+  return <div>
+    <div className='ida-desc'>
+      Our gated perimeter offers ample outdoor space where dogs can play, exercise, use the bathroom, or simply enjoy the fresh air at any time of day. For those who prefer indoor comfort, we provide three cozy bedrooms and a spacious living room to relax and unwind. <br /><br />We ensure your dog stays active with daily walks, often going out for two walks a day. At Ida Dog, we prioritize training and good manners, helping your dog become the best version of themselves. Whether your pup is just learning to sit on command or already excels at picking up new tricks, we tailor challenges to their skill level, ensuring growth and enrichment every step of the way.
+    </div>
+    <div className='home_images'>
+      {/* <div>
+        <img src='/ida.png' className='ida' />
+      </div> */}
+      <div>
+        <img src='/ida2.png' className='ida' />
+      </div>
+      <div>
+        <img src='/ida3.png' className='ida' />
+      </div>
+      <div>
+        <img src='/ida4.png' className='ida' />
+      </div>
+      <div>
+        <img src='/ida5.png' className='ida' />
+      </div>
+    </div>
+  </div>;
 }
 
 export default App;
