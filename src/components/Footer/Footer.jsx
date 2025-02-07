e23e26e4a93db36f11486cd11dfde4d3ad15bf36
Jacob Silverman <57313642+jacobsilverman@users.noreply.github.com>
2025-02-06 21:18:56 -0800

    adding github to footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,6 +11,7 @@ const Footer = () => {
                 <SocialIcon className='social-icon-footer' network="facebook" url="https://facebook.com/jacob.silverman.73" />
                 <SocialIcon className='social-icon-footer' network="instagram" url="https://instagram.com/hardpersontofind" />
                 <SocialIcon className='social-icon-footer' network="linkedin" url="https://linkedin.com/in/jacobsilverman0" />
+                <SocialIcon className='social-icon-footer' network="github" url="https://github.com/jacobsilverman" />
             </div> 
         </footer>
     );
